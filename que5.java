b62c3207dd682e329cb698e48e3de43249e583bd
Anushka Napit <anushkanapit047@gmail.com>
2026-02-22 00:38:51 +0530

    Operators Problems

---
+++ que5.java
@@ -0,0 +1,11 @@
+public class que5 {
+    public static void main(String[] args) {
+       int x = 10, y = 5;
+
+       int exp1 = (y * (x / y + x / y));
+       int exp2 = (y * x / y + y * x / y);
+
+         System.out.println("Value of exp1: " + exp1);
+            System.out.println("Value of exp2: " + exp2);
+    }
+}
