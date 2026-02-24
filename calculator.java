aa37f50b1f5072468485502f30746d2e48a5d1bf
Anushka Napit <anushkanapit047@gmail.com>
2026-02-24 23:31:41 +0530

    switch case problems

---
+++ calculator.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class calculator {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number ");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number ");
+        int b = sc.nextInt();
+        System.out.println("Enter the operator (+, -, *, /) ");
+       
+        char operator = sc.next().charAt(0);
+        
+        switch(operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                if (b != 0) {
+                    System.out.println(a / b);
+                } else {
+                    System.out.println("Error: Division by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
