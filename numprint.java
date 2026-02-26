26817b637cbe414578e0eb87c2b954ec1ac5b8bc
Anushka Napit <anushkanapit047@gmail.com>
2026-02-27 00:18:53 +0530

    loops problems

---
+++ numprint.java
@@ -0,0 +1,10 @@
+public class numprint {
+    public static void main(String args[]) {
+        int counter = 1000;
+        while (counter > 0) {
+            System.out.println(counter);
+            counter--;
+        }
+    }
+
+}
---
+++ whileloop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class whileloop {
+    public static void main(String args[]) {
+        double counter = 0;
+        while (counter < 100)
+        {
+            System.out.println("hello Anushka");
+            counter++;
+        }
+    
+}
+}
